cf5092cf9b225e14b66ce1e457c1eae9656604ac
Loy Neil <loyneil92@gmail.com>
2025-02-10 17:31:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import { Home } from "./Home";
 import { Skills } from "./Skills";
 import { Project } from "./Project";
 import { Experience } from "./Experience";
+import { ChatBot } from "./ChatBot";
+import { CrmEnhancement } from "./CrmEnhancement";
+import { Escalation } from "./Escalation";
 
 function App() {
   const location = useLocation();
@@ -12,7 +15,7 @@ function App() {
   return (
     <div
       className={`grid grid-cols-1 md:grid md:grid-cols-2 place-items-center ${
-        location.pathname === "/home" || "/project" ? "md:overflow-hidden overflow-auto" : "overflow-auto"
+        location.pathname === "/home" || location.pathname === "/project" || location.pathname === "/project/chatbot" || location.pathname === "/project/crm" ? "md:overflow-hidden overflow-auto" : "overflow-auto"
       }`}
     >
       <NavBar />
@@ -23,6 +26,9 @@ function App() {
           <Route path="/experience" element={<div className="h-full overflow-auto"><Experience /></div>}/>
           <Route path="/skills" element={<Skills />}/>
           <Route path="/project" element={<Project />} />
+          <Route path="/project/chatbot" element={<ChatBot />} />
+          <Route path="/project/crm" element={<CrmEnhancement />} />
+          <Route path="/project/escalation" element={<Escalation />} />
         </Routes>
       </div>
     </div>
