c1b85e2de98ec40ebd0719b11ddaa6ca6f47022e
Loy Neil <loyneil92@gmail.com>
2025-02-10 17:31:45 +0530

    Update Project.jsx

--- src/Project.jsx
+++ src/Project.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import FancyText from "@carefully-coded/react-text-gradient";
 import { motion } from "framer-motion";
+import { Link, Outlet } from "react-router-dom";
 
 export function Project() {
   return (
     <motion.div
-    initial={{y:-1000}}
-    animate={{y:0}}
-    transition={{duration:1}}>
+      initial={{ y: -1000 }}
+      animate={{ y: 0 }}
+      transition={{ duration: 2 }}
+    >
       <FancyText
         gradient={{ from: "#FF6F61", to: "#6B5B95", type: "linear" }}
         animateTo={{ from: "#6B5B95", to: "#FF6F61" }}
@@ -24,35 +26,41 @@ export function Project() {
         </h1>
         <div className="grid grid-cols-1 p-2 place-items-center md:grid-cols-4 md:flex md:gap-20 md:-ml-5 md:-mt-3">
           <div>
-            <p className="text-center mb-2 font-semibold p-2">ChatBOT</p>
+            <p className="text-center mb-2 font-semibold p-2">
+              <Link to="chatbot">ChatBOT</Link>
+            </p>
+            <Link to="chatbot">
             <motion.img
-            initial={{x:1000}}
-            animate={{x:0}}
-            transition={{duration:1}}
+              initial={{ x: 1000 }}
+              animate={{ x: 0 }}
+              transition={{ duration: 2 }}
               className="w-20 h-30 p-2 md:w-30 md:h-40"
               src="/chatbot.jpg"
               alt="chatbot"
             />
+            </Link>
           </div>
           <div>
             <p className="text-center mb-2 font-semibold p-2">
-              CRM Enhancement
+              <Link to="crm">CRM Enhancement</Link>
             </p>
+            <Link to="crm">
             <motion.img
-            initial={{x:1000}}
-            animate={{x:0}}
-            transition={{duration:1}}
+              initial={{ x: 1000 }}
+              animate={{ x: 0 }}
+              transition={{ duration: 2 }}
               className="w-40 h-30 p-2 md:w-50 md:h-40"
               src="/crm.png"
               alt="crm"
             />
+            </Link>
           </div>
           <div>
             <p className="text-center mb-2 font-semibold p-2">AHT Reduction</p>
             <motion.img
-            initial={{x:1000}}
-            animate={{x:0}}
-            transition={{duration:1}}
+              initial={{ x: 1000 }}
+              animate={{ x: 0 }}
+              transition={{ duration: 2 }}
               className="w-30 h-30 p-2 md:w-40 md:h-40"
               src="/aht.jpg"
               alt="AHT Reduction"
@@ -62,14 +70,16 @@ export function Project() {
             <p className="text-center mb-2 font-semibold p-2">
               Escalation Reduction
             </p>
+            <Link to="escalation">
             <motion.img
-            initial={{x:1000}}
-            animate={{x:0}}
-            transition={{duration:1}}
+              initial={{ x: 1000 }}
+              animate={{ x: 0 }}
+              transition={{ duration: 2 }}
               className="w-40 h-30 p-2 md:w-40 md:h-40"
               src="/escalation.jpeg"
               alt="escalation"
             />
+            </Link>
           </div>
         </div>
       </div>
@@ -83,9 +93,9 @@ export function Project() {
               Quality Dashboard
             </p>
             <motion.img
-            initial={{x:1000}}
-            animate={{x:0}}
-            transition={{duration:1}}
+              initial={{ x: 1000 }}
+              animate={{ x: 0 }}
+              transition={{ duration: 2 }}
               className="w-40 h-30 p-2 md:w-50 md:h-40 md:-mt-2"
               src="/quality.png"
               alt="quality"
@@ -94,9 +104,9 @@ export function Project() {
           <div>
             <p className="text-center mb-2 font-semibold p-2">Portfolio</p>
             <motion.img
-            initial={{x:1000}}
-            animate={{x:0}}
-            transition={{duration:1}}
+              initial={{ x: 1000 }}
+              animate={{ x: 0 }}
+              transition={{ duration: 2 }}
               className="w-40 h-30 p-2 md:w-50 md:h-40 md:-mt-2"
               src="/portfolio.gif"
               alt="portfolio"
@@ -105,9 +115,9 @@ export function Project() {
           <div>
             <p className="text-center mb-2 font-semibold p-2">WeatherApp</p>
             <motion.img
-            initial={{x:1000}}
-            animate={{x:0}}
-            transition={{duration:1}}
+              initial={{ x: 1000 }}
+              animate={{ x: 0 }}
+              transition={{ duration: 2 }}
               className="w-40 h-30 p-2 md:w-50 md:h-40 md:-mt-2"
               src="/weather.png"
               alt="weather"
@@ -115,6 +125,7 @@ export function Project() {
           </div>
         </div>
       </div>
+      <Outlet />
     </motion.div>
   );
 }
